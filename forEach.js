71233b3e6872ed5a41b4dfe5eb95f6c814021a4f
Arturianez <artur.zank@gmail.com>
2023-08-31 21:30:59 -0400

    map second

--- forEach.js
+++ forEach.js
@@ -1,4 +1,4 @@
- const arrayForEach = [1,4,3,'one', 'two','three']
+//  const arrayForEach = [1,4,3,'one', 'two','three']
 // let sum = 0;
 //   arrayForEach.forEach(function(item){
 //     if(typeof item !== 'string') {
@@ -6,11 +6,11 @@
 //     }
 //   })
 // console.log(sum)
-let number = 0;
-arrayForEach.forEach(function(item){
-    if(typeof item !== 'string') {
-        if(item % 2  === 0) {
-            console.log(item)
-        }
-    }
-})
+// let number = 0;
+// arrayForEach.forEach(function(item){
+//     if(typeof item !== 'string') {
+//         if(item % 2  === 0) {
+//             console.log(item)
+//         }
+//     }
+// })
---
+++ map.js
@@ -0,0 +1,6 @@
+const names = ['John', 'Jane', 'Michael', 'Emily'];
+
+const fullName = names.map(function(item){
+    return item + ' Smith'
+})
+console.log(fullName)
