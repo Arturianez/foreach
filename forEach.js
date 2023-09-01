3bfd40826f4d449faca2eb303a2a58d296d0eb5c
Arturianez <artur.zank@gmail.com>
2023-08-31 21:07:58 -0400

    second push

---
+++ forEach.js
@@ -0,0 +1,16 @@
+ const arrayForEach = [1,4,3,'one', 'two','three']
+// let sum = 0;
+//   arrayForEach.forEach(function(item){
+//     if(typeof item !== 'string') {
+//         sum = sum + item
+//     }
+//   })
+// console.log(sum)
+let number = 0;
+arrayForEach.forEach(function(item){
+    if(typeof item !== 'string') {
+        if(item % 2  === 0) {
+            console.log(item)
+        }
+    }
+})
